8b211c58617954e0fb53ade4bf0b85ae31ebc4f2
NhatTaiC <nhattai0501@gmail.com>
2024-05-25 04:33:18 +0700

    Modified & Aded (Finals 25/05/2024)

---
+++ Project_IOS_Group6/Src_Code/Sang_IOS/QuanLyChiTieuCaNhan2024/QuanLyChiTieuCaNhan2024/Controls/ChiTieuCell.swift
@@ -0,0 +1,39 @@
+//
+//  ChiTieuCell.swift
+//  QuanLyChiTieuCaNhan2024
+//
+//  Created by  User on 22.05.2024.
+//
+
+import UIKit
+
+class ChiTieuCell: UITableViewCell {
+    
+    // MARK: PROPERTIES
+    @IBOutlet weak var tenCT: UILabel!
+    
+    @IBOutlet weak var ngayTao: UILabel!
+    
+    @IBOutlet weak var soTien: UILabel!
+    
+    var onTap:UITapGestureRecognizer?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+        if onTap != nil {
+            onTap!.delegate = self
+        }
+    }
+
+    // Dinh nghia ham uy quyen cho doi tuong UITapGestureRecognizer
+    override func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return (touch.view == self.contentView)
+    }
+}
---
+++ Project_IOS_Group6/Src_Code/Sang_IOS/QuanLyChiTieuCaNhan2024/QuanLyChiTieuCaNhan2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuanLyChiTieuCaNhan2024
+//
+//  Created by  User on 22.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
